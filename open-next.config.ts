179d969cac3da6fe7bff9f9efbc7a184b2d1f0af
stevenvlr <steven.veiller.skillbase@gmail.com>
2026-03-21 22:01:37 +0100

    fix: configure worker entrypoint

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,7 +1,5 @@
 import { defineCloudflareConfig } from '@opennextjs/cloudflare';
 
 export default defineCloudflareConfig({
-  worker: {
-    entrypoint: ".open-next/worker.js",
-  },
+  outputDir: ".open-next",
 });
